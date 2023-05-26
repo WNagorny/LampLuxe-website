eeb2b1f7cfdea59193bb736013c4b16eff17927b
WNagorny <walery.nagorny@gmail.com>
2023-05-26 11:15:14 +0200

    cleaning the code

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const lightBtn = document.querySelector('.menu-btn')
 const light = document.querySelector('.light')
-const activeMode = document.querySelector('.active-mode')
 
 const toggleLight = () => {
    light.classList.toggle('activelight')
    lightBtn.classList.toggle('activebtn')
-   activeMode.classList.toggle('activeModeOn')
 }
 
 lightBtn.addEventListener('click', toggleLight)
